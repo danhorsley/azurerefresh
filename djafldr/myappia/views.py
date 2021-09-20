c72800ae2766300c5d0ddbcbb19b31274a2cf2f5
danhorsley <danielhorsley@me.com>
2021-09-20 16:21:19 +0100

    cleaning up subtitle of viz

--- djafldr/myappia/views.py
+++ djafldr/myappia/views.py
@@ -20,11 +20,19 @@ def index(request):
     return HttpResponse("Hello everyone. My app is live")
 
 def query2(request):
+    
     try:
+        if request.POST['timeperiod'] == "all time":
+            new_time_period = "all time"
+        else:
+            new_time_period = f"last {request.POST['timeperiod']}"
         html_plot = sales_over_time_chart(request.POST['measure'], request.POST['timeperiod'],
                                         request.POST['title'], request.POST['timesplit'])
+        default_sub = f"{request.POST['timesplit'].capitalize()} {request.POST['measure']} of {request.POST['title']} over {new_time_period}"
     except:
         html_plot = sales_over_time_chart()
+        default_sub = "Sales of all titles since inception"
+
     title = list(set(DailyData.objects.values_list('itemname', flat=True))) #filter1 for title 
     timesplit = ['daily', 'by weekday', 'by week', 'by month']
     time_period = ['all time', '7d', '30d', '90d', '180d'] #filter for time period
@@ -36,7 +44,8 @@ def query2(request):
     return render(request, 'query2.html', {'titles' : title , 'timesplit' : timesplit ,
                                                 'time_period' : time_period,
                                                 'measures' : measure,
-                                                'html_plot' : html_plot
+                                                'html_plot' : html_plot,
+                                                "default_sub" : default_sub
                                                 })
 
 def homepage(request):
